8f65f576a6fae11bb1c23e562b24d0253e4ccfe8
Kevin Abelgas <kev.abelgas@gmail.com>
2023-03-21 20:26:40 +0800

    search query fix

--- pages/search/[result].js
+++ pages/search/[result].js
@@ -7,17 +7,16 @@ import SearchResult from "@/components/search";
 export async function getServerSideProps(context) {
     const searchInput = context.query.result.toLowerCase();
     const usersRef = collection(db, "users");
-    const q = query(usersRef, where("lowerCaseDisplayName", ">=", searchInput), where("lowerCaseDisplayName", "<=", searchInput+"\uf8ff"));
-    const qSnapshot = await getDocs(q);
-
+    const qSnapshot = await getDocs(usersRef);
     const results = []
 
     qSnapshot.forEach(async (userDoc) => {
         results.push({uid: userDoc.id, data: userDoc.data()});
     })
+    console.log(results.length)
 
     return {
-        props: {results}
+        props: {results, searchInput}
     }
     
 }
@@ -28,13 +27,14 @@ export default function search(props) {
             {props.results.length < 1 ? <h2> No results found! </h2> :
                 (
                     props.results.map((user, index) => {
-                        return (
-                            <SearchResult
-                            key={index}
-                            uid={user.uid}
-                            data={user.data}
-                            />
-                        );
+                        if (user.data.lowerCaseDisplayName.includes(props.searchInput))
+                            return (
+                                <SearchResult
+                                key={index}
+                                uid={user.uid}
+                                data={user.data}
+                                />
+                            );
                     })
                 )
             }
