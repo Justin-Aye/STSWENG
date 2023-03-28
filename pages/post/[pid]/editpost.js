809f4789c966782bd3f4770f0de66d23bacfbc6c
Justin Ayuyao <balcera85@gmail.com>
2023-03-28 11:23:27 +0800

    Other commits

---
+++ pages/post/[pid]/editpost.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import EditPost from "../../../components/editPost";
+
+export default function editpost() {
+    return (
+        <>
+            <EditPost />
+        </>
+    )
+}
